5eb980d56c33af6a0aa30a10cb4bf587279f0c9d
Zalexanninev15 <blue.shark@disroot.org>
2021-05-04 16:35:49 +0300

    Improvements

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -1,14 +1,6 @@
-﻿#region Using directives
-
-using System;
+﻿using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
-
-#endregion
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("TelegramServerStatusBot")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -17,15 +9,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("GPL-3.0 License")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// This sets the default COM visibility of types in the assembly to invisible.
-// If you need to expose a type to COM, use [ComVisible(true)] on that type.
 [assembly: ComVisible(false)]
-
-// The assembly version has following format :
-//
-// Major.Minor.Build.Revision
-//
-// You can specify all the values or you can use the default the Revision and 
-// Build Numbers by using the '*' as shown below:
 [assembly: AssemblyVersion("1.3.0.0")]
