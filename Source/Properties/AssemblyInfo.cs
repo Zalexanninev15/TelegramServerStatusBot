c216c2cf3bb4ac1ffda1b69c920f1904dd1bf263
Zalexanninev15 <blue.shark@disroot.org>
2021-05-23 14:03:39 +0300

    Version 1.4
    The first and last update of this year!
    
    Changes:
    - Slightly changed the way to get an IP address
    - Fixed bugs
    - Code improvements and removal of unnecessary things

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("TelegramServerStatusBot")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Bot for sending the status of the server (PC) to the Telegram messenger")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TelegramServerStatusBot")]
@@ -10,4 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
