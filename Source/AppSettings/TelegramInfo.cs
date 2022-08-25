60c000ae5e672d2071b8a7b81fc8838546fb5533
ali tamim <amoh331@gmail.com>
2022-08-25 18:32:39 +0300

    update

---
+++ Source/AppSettings/TelegramInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramServerStatusBot.AppSettings
+{
+    public class TelegramInfo
+    {
+        public string BotToken { get; set; }
+        public string UserID { get; set; }
+    }
+}
